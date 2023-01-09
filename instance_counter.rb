caf32b4eddaf40c9d308b41ffa450c20396f1385
izulolov <izulolov@mail.ru>
2023-01-09 10:23:30 +0300

    seven

--- instance_counter.rb
+++ instance_counter.rb
@@ -1,20 +1,15 @@
 module InstanceCounter
-  # такой параметр (place) нужен, так как на место этого параметра будет передаватся
-  # сам класс куда мы подключаем этот модуль. Я абсолютно уверен, что я всё правильно делаю 
-  def self.included(place)
-    place.extend ClassMethods
-    place.include InstanceMethods
+  def self.included(base)
+    base.extend ClassMethods
+    base.include InstanceMethods
   end
   module ClassMethods
-
     def instances
       @instances ||= 0
-      @instances += 1
     end
   end
 
   module InstanceMethods
-
     protected
 
     def register_instance
